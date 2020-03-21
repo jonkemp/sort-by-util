288144bc8fac43faf77de16417633ed8307ed726
Jonathan Kemp <kempdogg@gmail.com>
2020-03-21 00:21:06 -0400

    Add dependency for map.

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
 const pluck = require('pluck-util');
-const isArrayLike = require('./lib/is-array-like');
+const map = require('map-plus');
 const { cb } = require('./lib/index');
-
-const map = function(obj, iteratee, context) {
-	iteratee = cb(iteratee, context);
-	var keys = !isArrayLike(obj) && Object.keys(obj),
-		length = (keys || obj).length,
-		results = Array(length);
-	for (var index = 0; index < length; index++) {
-		var currentKey = keys ? keys[index] : index;
-		results[index] = iteratee(obj[currentKey], currentKey, obj);
-	}
-	return results;
-};
 
 const sortBy = (obj, iteratee, context) => {
 	let index = 0;
+
 	iteratee = cb(iteratee, context);
+
 	return pluck(map(obj, (value, key, list) => ({
 		value,
 		index: index++,
@@ -24,10 +14,12 @@ const sortBy = (obj, iteratee, context) => {
 	})).sort((left, right) => {
 		const a = left.criteria;
 		const b = right.criteria;
+
 		if (a !== b) {
 			if (a > b || a === void 0) return 1;
 			if (a < b || b === void 0) return -1;
 		}
+
 		return left.index - right.index;
 	}), 'value');
 };
